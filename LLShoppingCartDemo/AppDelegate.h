85b80401676e65884de6fd84b460c236d7a8ab8a
Leo9073 <Leo@leomatomacbook-pro.local>
2016-11-18 14:35:56 +0800

    Initial Commit

---
+++ LLShoppingCartDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLShoppingCartDemo
+//
+//  Created by Leo on 11/18/16.
+//  Copyright © 2016 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLShoppingCartDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLShoppingCartDemo
+//
+//  Created by Leo on 11/18/16.
+//  Copyright © 2016 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
